30117ff9c768144472691618780217f4e2fe91b1
Basile Garcia <Basile.Garcia@unige.ch>
2023-12-06 13:54:58 +0100

    update

--- _static/js/main.mjs
+++ _static/js/main.mjs
@@ -1,7 +1,34 @@
-import { cardDesc } from './card-desc.js';
+// import { cardDesc } from './card-desc.js';
 
 // global variables 
 var startTime = Date.now();
+export const cardDesc = {
+    savethechildren: `Save the Children believes every child deserves a future. In the United States and around the world,
+    we work to give children a healthy start in life, the opportunity to learn and protection from harm.
+    We ensure children's unique needs are met and their voices are heard. We deliver lasting results for
+    millions of children, including those hardest to reach.`,
+    unicef: `Over eight decades, the United Nations Children&#39;s Fund (UNICEF) has built an unprecedented global
+    support system for the world's children. UNICEF works to deliver the essentials that give every child
+    an equitable chance in life: health care and immunizations, safe water and sanitation, nutrition,
+    education, emergency relief and more.`,
+    wwf: `World Wildlife Fund (WWF) mission is the conservation of nature. Using the best available scientific
+    knowledge and advancing that knowledge where we can, we work to preserve the diversity and
+    abundance of life on Earth. We are committed to reversing the degradation of our planet&#39;s natural
+    environment and to building a future in which human needs are met in harmony with nature.`,
+    thenatureconservancy: `The Nature Conservancy is a leading conservation organization working around the world to protect
+    ecologically important lands and waters for nature and people. We address threats to conservation
+    involving climate change, fire, fresh water, forests, invasive species, and marine ecosystems. We use
+    a science-based approach, and we pursue non-confrontational, pragmatic solutions to conservation
+    challenges.`,
+    cancerresearchuk: `
+    Cancer Research UK is the world’s largest independent cancer research institute. It works on how to
+    prevent, diagnose and treat cancer and has benefitted millions of lives over the past 120 years.
+    `, 
+    alzheimersresearchuk: `
+    Alzheimer’s Research UK is the UK’s largest dementia charity, dedicated to funding research to
+    understand, diagnose, reduce risk and treat the diseases that cause dementia.
+    `
+}
 
 
 function setActiveCurrentStep(id) {
